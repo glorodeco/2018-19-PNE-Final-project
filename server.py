95d16024616c7d864f76c2e01e937733ee0c2dd5
gloro <glorodeco@gmail.com>
2019-05-06 16:44:51 +0200

    Final Practice

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import http.server
 import socketserver
 import termcolor
 import requests, sys
+from Seq import Seq
 
 PORT = 8000
 
@@ -24,19 +25,19 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         termcolor.cprint(reqpath, 'blue')
 
         try:
-
+            contents=""
             if reqpath == '/main_page' or reqpath == '/':
 
                 with open("main_page.html", "r") as f:
                     contents = f.read()
                     f.close()
 
-            elif self.path.startswith('/list_species'):
+            elif self.path.startswith('/listSpecies'):
                 with open("list_species.html", "r") as f:
                     contents = f.read()
                     f.close()
 
-                    if self.path.startswith('/list_species?limit='):
+                    if self.path.startswith('/listSpecies?limit='):
 
                         #PONER DE DONDE ES EL CÓDIGO
                         server = "http://rest.ensembl.org"
@@ -100,11 +101,11 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
 
 
 
-            elif self.path.startswith('/chromosome_lenght'):
+            elif self.path.startswith('/chromosomeLength'):
                 with open("chromosome_lenght.html", "r") as f:
                     contents = f.read()
                     f.close()
-                    if self.path.startswith('/chromosome_length?species='):
+                    if self.path.startswith('/chromosomeLength?specie='):
 
                         msg_split = self.path.split('=')
 
@@ -128,19 +129,19 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                             if chromo == i['name']:
                                 length = i['length']
 
-                            with open("species_limit.html", "r") as f:
-                                contents = f.read()
-                                sentence = 'The length of the chromosome ' + chromo + ' is: ' ' from the species ' + msg_split2[1] + ' is ' + str(length)
-                                contents = contents.replace('#', sentence)
+                                with open("chromo_limit.html", "r") as f:
+                                    contents = f.read()
+                                    sentence = 'The length of the chromosome ' + chromo + ' of the species ' + msg_split2[1] + ' is: ' + str(length)
+                                    contents = contents.replace('#', sentence)
 
 
 
-            elif self.path.startswith('/gene_seq'):
+            elif self.path.startswith('/geneSeq'):
                 with open("gene_seq.html", "r") as f:
                     contents = f.read()
                     f.close()
 
-                    if self.path.startswith('/gene_seq?gene='):
+                    if self.path.startswith('/geneSeq?gene='):
 
                         msg_split = self.path.split('=')
 
@@ -153,11 +154,11 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                         info = r.json()
 
                         if msg_split[1] == info['display_name']:
-                            msg_split[1] = info['id']
+                            name = info['id']
 
                         server = "http://rest.ensembl.org"
 
-                        ext = "/sequence/id/{}?".format(msg_split[1])
+                        ext = "/sequence/id/{}?".format(name)
 
                         r = requests.get(server + ext, headers={"Content-Type": "application/json"})
 
@@ -172,11 +173,11 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
 
 
 
-            elif self.path.startswith('/gene_info'):
+            elif self.path.startswith('/geneInfo'):
                 with open("gene_info.html", "r") as f:
                     contents = f.read()
                     f.close()
-                if self.path.startswith('/gene_info?gene='):
+                if self.path.startswith('/geneInfo?gene='):
 
                     msg_split = self.path.split('=')
 
@@ -187,13 +188,13 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                     r = requests.get(server + ext, headers={"Content-Type": "application/json"})
 
                     info = r.json()
-
+                    name=""
                     if msg_split[1] == info['display_name']:
-                        msg_split[1] = info['id']
+                        name = info['id']
 
                     server = "http://rest.ensembl.org"
 
-                    ext = "/lookup/id/{}?".format(msg_split[1])
+                    ext = "/lookup/id/{}?".format(name)
 
                     r = requests.get(server + ext, headers={"Content-Type": "application/json"})
 
@@ -209,8 +210,103 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
 
                     with open("gene_info_gene.html", "r") as f:
                             contents = f.read()
-                            text= 'The start of the human gene ' + msg_split[1] + ' is ' + str(start) + '</p></p>' + 'The end of the human gene ' + msg_split[1] + ' is ' + str(end) + '</p></p>' + 'The id of the human gene ' + msg_split[1] + ' is ' + str(id) +'</p></p>' + 'The chromosome of the human gene ' + msg_split[1] + ' is ' + str(chromo)
+                            text= 'The start of the human gene ' + msg_split[1] + ' is: ' + str(start) + '</p></p>' + 'The end of the human gene ' + msg_split[1] + ' is: ' + str(end) + '</p></p>' + 'The id of the human gene ' + msg_split[1] + ' is: ' + str(id) +'</p></p>' + 'The gen ' + msg_split[1] + ' is in  the chromosome: ' + str(chromo)
                             contents = contents.replace('#', text)
+
+
+
+
+            elif self.path.startswith('/geneCalc'):
+                with open("gene_calc.html", "r") as f:
+                    contents = f.read()
+                    f.close()
+
+                if self.path.startswith('/geneCalc?gene='):
+
+                    msg_split = self.path.split('=')
+
+                    server = "http://rest.ensembl.org"
+
+                    ext = "/lookup/symbol/homo_sapiens/{}?".format(msg_split[1])
+
+                    r = requests.get(server + ext, headers={"Content-Type": "application/json"})
+
+                    info = r.json()
+                    name=''
+                    if msg_split[1] == info['display_name']:
+                        name = info['id']
+
+                    server = "http://rest.ensembl.org"
+
+                    ext = "/sequence/id/{}?".format(name)
+
+                    r = requests.get(server + ext, headers={"Content-Type": "application/json"})
+
+                    info = r.json()
+
+                    gene_sequence = info['seq']
+
+                    seq = Seq(gene_sequence)
+
+                    length = str(seq.len())
+
+                    percentageA = str(seq.perc('A'))
+                    percentageT = str(seq.perc('T'))
+                    percentageG = str(seq.perc('G'))
+                    percentageC = str(seq.perc('C'))
+
+                    sentence= 'The length of the sequence of the human gene ' + msg_split[1] + ' is: ' + length + '</p></p>'
+
+                    per_a = 'The percentage of the base A in the sequence of the human gene: ' + msg_split[
+                        1] + ' is ' + percentageA + '%' + '</p></p>'
+
+                    per_t = 'The percentage of the base T in the sequence of the human gene: ' + msg_split[
+                        1] + ' is ' + percentageT + '%' + '</p></p>'
+
+                    per_g = 'The percentage of the base G in the sequence of the human gene: ' + msg_split[
+                        1] + ' is ' + percentageG + '%' + '</p></p>'
+
+                    per_c = 'The percentage of the base C in the sequence of the human gene: ' + msg_split[1] + ' is ' + percentageC + '%'
+
+                    text = sentence + per_a + per_t + per_g + per_c
+                    with open("gene_calResult.html", "r") as f:
+                        contents = f.read()
+                        contents = contents.replace('#', text)
+
+                # Return the names of the genes located in the chromosome "chromo" from the start to end positions
+
+            elif self.path.startswith('/geneList'):
+                with open("gene_list.html", "r") as f:
+                    contents = f.read()
+                    f.close()
+
+                if self.path.startswith('/geneList?chromo='):
+
+                    msg_split = self.path.split('=')
+
+                    msg_split2 = []
+
+                    for i in msg_split:
+                        msg_split2 += i.split('&')
+
+                    server = "http://rest.ensembl.org"
+
+                    ext = "/overlap/region/human/" + str(msg_split2[1]) + ":" + msg_split2[3] + "-" + msg_split2[
+                        5] + "?feature=gene;feature=transcript;feature=cds;feature=exon"
+
+                    r = requests.get(server + ext, headers={"Content-Type": "application/json"})
+
+                    inform = r.json()
+
+                    sentence = 'The genes in the chromosome ' + msg_split2[1] + ' of the homo sapiens ' + ' between ' + msg_split2[3] + ' and ' + msg_split2[5] + ' are: '
+                    for i in inform:
+                        sentence += '<p>' + i['id'] + '<p>'
+
+                    with open("gene_listRes.html", "r") as f:
+                            contents = f.read()
+                            contents = contents.replace('#', sentence)
+
+
 
             self.send_response(200)  # -- Status line: OK!
 
@@ -228,29 +324,24 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             return
 
 
+
         except KeyError:
-            f = open("error.html", 'r')
-
+            f = open('error.html', 'r')
             contents = f.read()
-            self.send_response(200)
 
-            self.send_header('Content-Type', 'text/html')
+            self.send_response(200)  # -- Status line: OK!
 
-            self.send_header('Content-Length', len(str.encode(contents)))
+            # Define the content-type header:
+            self.send_header("Content-Type", "text/html\r\n")
 
+            # The header is finished
             self.end_headers()
 
+            # Send the response message
+            self.wfile.write(str.encode(contents))
 
 
 
-
-        # -- Sending the body of the response message
-
-        #self.wfile.write(str.encode(contents))
-
-
-
-        #return
 
 
 
